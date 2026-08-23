f7bfe7b525b20aad1da19f2baac17c85a3ca4c63
brucehan <intragerhan@gmail.com>
2026-08-23 18:58:00 +0900

    동일한 주문인지 알 수 있도록 주문로직 수정

--- monolithic/src/main/java/com/example/monolithic/order/controller/dto/PlaceOrderRequest.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/dto/PlaceOrderRequest.java
@@ -5,23 +5,9 @@ import com.example.monolithic.order.application.dto.PlaceOrderCommand;
 import java.util.List;
 
 public record PlaceOrderRequest(
-        List<OrderItem> orderItems
+        Long orderId
 ) {
     public PlaceOrderCommand toPlaceOrderCommand() {
-        return new PlaceOrderCommand(
-                orderItems.stream()
-                        .map(item -> new PlaceOrderCommand.OrderItem(
-                                item.productId,
-                                item.quantity
-                        ))
-                        .toList()
-        );
-    }
-
-    public record OrderItem(
-            Long productId,
-            Long quantity
-    ) {
-
+        return new PlaceOrderCommand(orderId);
     }
 }
--- monolithic/src/main/java/com/example/monolithic/order/domain/Order.java
+++ monolithic/src/main/java/com/example/monolithic/order/domain/Order.java
@@ -10,10 +10,26 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
     public Order() {
+        status = OrderStatus.CREATED;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void complete() {
+        status = OrderStatus.COMPLETED;
+    }
+
+    public enum OrderStatus {
+        CREATED, COMPLETED
     }
 }
