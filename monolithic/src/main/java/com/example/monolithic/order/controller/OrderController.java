134e17cbecb8c57b036ab86b5cf0b024f78a0c85
brucehan <intragerhan@gmail.com>
2026-08-23 19:19:30 +0900

    Lock을 활용하여 주문로직이 1번만 수행되도록 변경

--- monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.monolithic.order.controller;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
     private final OrderService orderService;
+    private final RedisLockService redisLockService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, RedisLockService redisLockService) {
         this.orderService = orderService;
+        this.redisLockService = redisLockService;
     }
 
     @PostMapping("/order")
@@ -27,6 +30,16 @@ public class OrderController {
     public void placeOrder(
             @RequestBody PlaceOrderRequest request
     ) {
-        orderService.placeOrder(request.toPlaceOrderCommand());
+        String key = "order:monolithic:" + request.orderId();
+        boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+        if (!acquiredLock) {
+            throw new RuntimeException("락 획득에 실패하였습니다.");
+        }
+
+        try {
+            orderService.placeOrder(request.toPlaceOrderCommand());
+        } finally {
+            redisLockService.releaseLock(key);
+        }
     }
 }
