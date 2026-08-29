8c49283fdde69323e84e3f9990630cea4ef25d3b
brucehan <intragerhan@gmail.com>
2026-08-30 00:06:37 +0900

    TCC - Point Try API 추가

--- point/src/main/java/com/example/point/PointApplication.java
+++ point/src/main/java/com/example/point/PointApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PointApplication {
+public class  PointApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PointApplication.class, args);
--- point/src/main/java/com/example/point/domain/Point.java
+++ point/src/main/java/com/example/point/domain/Point.java
@@ -13,7 +13,21 @@ public class Point {
 
     private Long amount;
 
+    private Long reservedAmount;
+
+    @Version
+    private Long version;
+
     public Point() {
+    }
+
+    public void reserve(long reserveAmount) {
+        long reservableAmount = this.amount - reserveAmount;
+        if (reservableAmount < reserveAmount) {
+            throw new RuntimeException("금액이 부족합니다.");
+        }
+
+        this.reservedAmount += reserveAmount;
     }
 
     public Point(Long userId, Long amount) {
@@ -26,5 +40,21 @@ public class Point {
             throw new RuntimeException("잔액이 부족합니다.");
         }
         this.amount = this.amount - amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void confirm(Long reserveAmount) {
+        if (this.amount < reserveAmount) {
+            throw new RuntimeException("포인트가 부족합니다.");
+        }
+        if (this.reservedAmount < reserveAmount) {
+            throw new RuntimeException("예약된 금액이 부족합니다.");
+        }
+
+        this.amount -= reserveAmount;
+        this.reservedAmount -= reserveAmount;
     }
 }
