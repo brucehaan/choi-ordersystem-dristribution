6edfdc6006fdc8c29c12cb339e60242291d9e1a4
brucehan <intragerhan@gmail.com>
2026-08-30 12:53:12 +0900

    SAGA(Orchestration) - Order 생성 API 추가

---
+++ order/src/main/java/com/example/order/application/OrderService.java
@@ -0,0 +1,35 @@
+package com.example.order.application;
+
+import com.example.order.application.dto.CreateOrderCommand;
+import com.example.order.application.dto.CreateOrderResult;
+import com.example.order.domain.Order;
+import com.example.order.domain.OrderItem;
+import com.example.order.infrastructure.OrderItemRepository;
+import com.example.order.infrastructure.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+
+    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
+        this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+    }
+
+    @Transactional
+    public CreateOrderResult createOrder(CreateOrderCommand command) {
+        Order order = orderRepository.save(new Order());
+        List<OrderItem> orderItems = command.items()
+                .stream()
+                .map(item -> new OrderItem(order.getId(), item.productId(), item.quantity()))
+                .toList();
+        orderItemRepository.saveAll(orderItems);
+
+        return new CreateOrderResult(order.getId());
+    }
+}
